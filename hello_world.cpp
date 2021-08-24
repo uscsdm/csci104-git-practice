1d13f45f0b870881b1ea3a5684515bcc88e304f6
Samheeta Mistry <sdmistry@usc.edu>
2021-08-23 21:19:36 -0700

    Add hello_world.cpp with some code

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+//
+//  hello_world.cpp
+//  helloworld.cpp
+//
+//  Created by Samheeta Mistry on 8/23/21.
+//
+
+#include "hello_world.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+}
